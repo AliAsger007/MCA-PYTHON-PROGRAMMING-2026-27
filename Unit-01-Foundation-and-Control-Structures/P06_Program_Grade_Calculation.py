153771675e164a5be77a9add2a1cec4e333e8cd5
Ali Asger <104424221+AliAsger007@users.noreply.github.com>
2026-09-22 21:49:59 +0530

    Add grade calculation program based on percentage

---
+++ Unit-01-Foundation-and-Control-Structures/P06_Program_Grade_Calculation.py
@@ -0,0 +1,16 @@
+percentage = float(input("Enter percentage: "))
+
+if percentage < 0 or percentage > 100:
+    print("Invalid percentage")
+elif percentage >= 90:
+    print("Grade A")
+elif percentage >= 80:
+    print("Grade B")
+elif percentage >= 70:
+    print("Grade C")
+elif percentage >= 60:
+    print("Grade D")
+elif percentage >= 50:
+    print("Grade E")
+else:
+    print("Fail")
