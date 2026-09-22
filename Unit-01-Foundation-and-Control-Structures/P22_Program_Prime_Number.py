e23e547aa03c0d67768a44e40fb24ca908ee8191
Ali Asger <104424221+AliAsger007@users.noreply.github.com>
2026-09-22 22:56:55 +0530

    Add program to check for prime numbers

---
+++ Unit-01-Foundation-and-Control-Structures/P22_Program_Prime_Number.py
@@ -0,0 +1,15 @@
+n = int(input("Enter integer: "))
+prime = True
+
+if n < 2:
+    prime = False
+else:
+    for i in range(2, n):
+        if n % i == 0:
+            prime = False
+            break
+
+if prime:
+    print("Prime Number")
+else:
+    print("Not a Prime Number")
