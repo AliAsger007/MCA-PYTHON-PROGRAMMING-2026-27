f5dad82e6ff5bc15aacd04c25517828ef5a7afa5
Ali Asger <104424221+AliAsger007@users.noreply.github.com>
2026-09-22 21:01:54 +0530

    Add electricity bill calculation program

---
+++ Unit-01-Foundation-and-Control-Structures/P03_Program_Electricity_Bill.py
@@ -0,0 +1,12 @@
+units = int(input("Enter units: "))
+
+if units <= 100:
+    bill = units * 5
+elif units <= 200:
+    bill = 100 * 5 + (units - 100) * 7
+elif units <= 300:
+    bill = 100 * 5 + 100 * 7 + (units - 200) * 10
+else:
+    bill = 100 * 5 + 100 * 7 + 100 * 10 + (units - 300) * 12
+
+print("Electricity Bill:", bill)
