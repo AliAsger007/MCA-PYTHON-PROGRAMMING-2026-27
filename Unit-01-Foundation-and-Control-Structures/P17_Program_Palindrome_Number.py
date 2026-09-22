fb953d1de90197fde46290a065dce8e205e0714e
Ali Asger <104424221+AliAsger007@users.noreply.github.com>
2026-09-22 22:17:38 +0530

    Add palindrome number checker program

---
+++ Unit-01-Foundation-and-Control-Structures/P17_Program_Palindrome_Number.py
@@ -0,0 +1,13 @@
+n = int(input("Enter integer: "))
+original = n
+reverse = 0
+
+while n != 0:
+    digit = n % 10
+    reverse = reverse * 10 + digit
+    n //= 10
+
+if original == reverse:
+    print("Palindrome")
+else:
+    print("Not a Palindrome")
